7d17b7496f305bda099423ee2c7a35c6d600093d
Souless911 <A01228817@itesm.mx>
2021-10-24 20:14:10 -0500

    updated
    v0.12

--- lambda.js
+++ lambda.js
@@ -3,17 +3,34 @@ console.log('starting function pulling last copy')
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
 
-exports.handler = function(e,ctx,cb){
-   let scanningParameters= {
-       TableName:'Copy-Atlas',
-       Limit: 100
-   };
-   docClient.scan(scanningParameters,function(err,data){
-       if(err){
-           cb(err,null);
-       }else{
-           cb(null,data);
-       }
-   })
-};
-// este es el codigo que tiene la funcion lambda para sacar la informacion que tiene la base de datos.
+
+const createMessage = async(_params)=>{
+    try{
+        await docClient.put(_params).promise();
+    }catch(err){
+        console.log(err)
+        return err;
+    }
+}
+exports.handler = async (event) => {
+    try{
+        let form=event.body;
+        let params= {
+            Item:{ IdItem:3 , Cantidad:form.cantidad , Costo:form.costo , Nombre:form.Nombre},
+            TableName:'Copy-Atlas'
+        }
+
+         await createMessage(params);
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('added data'),
+    };
+    return response;
+    }catch(err){
+        const response = {
+        statusCode: 400,
+        body: JSON.stringify(err),
+    };
+        
+    }
+// este es el codigo que tiene la funcion lambda para meter informacion a la base de datos (COPIA)
